243535887e5946e5361e8919a93db650aed31b27
shaikhharisali <shaikhharisali16@gmail.com>
2026-06-27 16:42:21 +0500

    Challenges of Fundamentals-Part-2

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -32,82 +32,82 @@ GOOD LUCK 😀
 */
 
 // Challenge 1
-// const game = {
-//   team1: 'Bayern Munich',
-//   team2: 'Borrussia Dortmund',
-//   players: [
-//     [
-//       'Neuer',
-//       'Pavard',
-//       'Martinez',
-//       'Alaba',
-//       'Davies',
-//       'Kimmich',
-//       'Goretzka',
-//       'Coman',
-//       'Muller',
-//       'Gnarby',
-//       'Lewandowski',
-//     ],
-//     [
-//       'Burki',
-//       'Schulz',
-//       'Hummels',
-//       'Akanji',
-//       'Hakimi',
-//       'Weigl',
-//       'Witsel',
-//       'Hazard',
-//       'Brandt',
-//       'Sancho',
-//       'Gotze',
-//     ],
-//   ],
-//   score: '4:0',
-//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-//   date: 'Nov 9th, 2037',
-//   odds: {
-//     team1: 1.33,
-//     x: 3.25,
-//     team2: 6.5,
-//   },
-// };
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
 
-// // 1.
-// const [players1,players2] = game.players;
-// console.log(players1,players2);
+// 1.
+const [players1,players2] = game.players;
+console.log(players1,players2);
 
-// // 2.
-// const [gk1,...othersPalyers1] =players1;
-// console.log(gk1,othersPalyers1);
+// 2.
+const [gk1,...othersPalyers1] =players1;
+console.log(gk1,othersPalyers1);
 
-// const [gk2,...othersPalyers2] =players2;
-// console.log(gk2,othersPalyers2);
+const [gk2,...othersPalyers2] =players2;
+console.log(gk2,othersPalyers2);
 
-// // 3.
-// const allPlayers = [players1,players2];
-// console.log(allPlayers);
+// 3.
+const allPlayers = [players1,players2];
+console.log(allPlayers);
 
-// // 4.
-// const players1Final =[...players1,'Thiago', 'Coutinho' , 'Perisic']
-// console.log(players1Final);
+// 4.
+const players1Final =[...players1,'Thiago', 'Coutinho' , 'Perisic']
+console.log(players1Final);
 
-// // 5/
-// const {team1,x:draw,team2} = game.odds;
-// console.log(team1,draw,team2);
+// 5/
+const {team1,x:draw,team2} = game.odds;
+console.log(team1,draw,team2);
 
-// // 6.
-// const printGoals = function (...players) {
-//     console.log(players);
-//     console.log(`${players.length} goals were scored`);
-// }
+// 6.
+const printGoals = function (...players) {
+    console.log(players);
+    console.log(`${players.length} goals were scored`);
+}
 
-// printGoals(...game.scored)
+printGoals(...game.scored)
 
-// 7.
+7.
 
-// team1 < team2 && console.log('Team 1 is more likely to win');
-// team1 > team2 && console.log('Team 2 is more likely to win');
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
 
 
 ///////////////////////////////////////
@@ -177,26 +177,26 @@ GOOD LUCK 😀
 //   },
 // };
 
-// // 1.
-// game.scored.forEach((goal,i)=>{
-//     console.log(`Goal ${i+1} : ${goal}`);
-// })
+// 1.
+game.scored.forEach((goal,i)=>{
+    console.log(`Goal ${i+1} : ${goal}`);
+})
 
-// // 2.
-// const odds = Object.values(game.odds);
-// console.log(odds);
-// let average = 0;
-// for (const odd of odds) {
-//     average =+ odd
-// }
-// average/=odds.length
-// console.log(average);
+// 2.
+const odds = Object.values(game.odds);
+console.log(odds);
+let average = 0;
+for (const odd of odds) {
+    average =+ odd
+}
+average/=odds.length
+console.log(average);
 
-// // 3.
-// for(const [team,odd] of Object.entries(game.odds)){
-//      const teamStr = team === 'x' ? 'draw': `victory ${game[team]}` ;
-//      console.log(`Odd of ${teamStr} : ${odd} `);     
-//}
+// 3.
+for(const [team,odd] of Object.entries(game.odds)){
+     const teamStr = team === 'x' ? 'draw': `victory ${game[team]}` ;
+     console.log(`Odd of ${teamStr} : ${odd} `);     
+}
 
 /* 
 Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
